f7f5133d940ce4eff803eb85b3369fb1891fde91
Michielvdz <r0750793@student.thomasmore.be>
2021-12-26 15:09:10 +0100

    first commit

---
+++ src/main/java/com/example/anime_characters/AnimeCharactersApplication.java
@@ -0,0 +1,13 @@
+package com.example.anime_characters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeCharactersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeCharactersApplication.class, args);
+    }
+
+}
